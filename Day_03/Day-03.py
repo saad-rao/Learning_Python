ff35e50575348deb3465c091c77e5e03f8e5c31f
Saad_Naseem <raosad786@gmail.com>
2025-02-22 21:38:38 +0500

    Add data types demonstration in Day-03.py

---
+++ Day_03/Day-03.py
@@ -0,0 +1,24 @@
+#DATA tYPES IN PYTHON  
+
+# 1.Numeric tYPES
+# Python has three main numeric types:
+# a.Integer(int)
+# b.Floating-point(float)
+# c.Complex (complex)
+
+
+num_init:int = 20
+print(type(num_init), "num_init =",num_init)
+
+
+num_float: float = 3.14
+#num_float: float = .14
+
+print(type(num_float), " num_float = ", num_float)  # <class 'float'>
+
+
+
+num_complex: complex = 2 + 3j
+
+print(type(num_complex), " num_complex = ", num_complex)  # <class 'complex'>
+
